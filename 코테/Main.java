9ec3260ef37ebdba41e82aa4244dd84abb99b3e6
leewoojin <lwj061947@gmail.com>
2025-08-17 14:36:37 +0900

    백준 2903 중앙 이동 알고리즘

--- 코테/Main.java
+++ 코테/Main.java
@@ -5,25 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        
         int T = sc.nextInt();
-        for(int i = 0; i < T; i++) {
-            int[] answer = new int[4];
-            int num = sc.nextInt();
-            answer[0] = num / 25;
-            num %= 25;
-            answer[1] = num / 10;
-            num %= 10;
-            answer[2] = num / 5;
-            num %= 5;
-            answer[3] = num / 1;
-            num %= 1;
+        int ans = 0;
+        int dot = 2;
 
-            System.out.println(answer[0] + " " + answer[1] + " " + answer[2] + " " + answer[3]);
+        for(int i =T; i > 0; i--) {
+        dot += (int)Math.pow(2, (i-1));
         }
-        
+
+        ans += (int)Math.pow(dot, 2);
+        System.out.println(ans);
         sc.close();
     }
 }
